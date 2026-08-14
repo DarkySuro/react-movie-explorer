d243b3a7ecd8b2b57b03914302597d2c3999f96f
DarkySuro <janasurojit1996@gmail.com>
2026-08-14 21:51:08 +0530

    Use useParams to dynamically populate MovieDetails component

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,67 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+const apikey = import.meta.env.VITE_OMDB_API_KEY;
 
 export default function MovieDetails() {
   const { id } = useParams();
-  return <h1>Showing Movie: { id }</h1>;
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchMovieDetails() {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${apikey}&i=${id}`,
+        );
+
+        if (!res.ok) throw new Error("Failed to fetch details!");
+        const data = await res.json();
+
+        if (data.Response === "False")
+          throw new Error("Wrong Movie or does not exist!");
+
+        setMovie(data);
+      } catch (e) {
+        console.log(e.message);
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMovieDetails();
+  }, [id]);
+
+  return (
+    <div>
+      <Link to="/">Back to Search</Link>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h1 style={{ color: "red" }}>{error}</h1>
+      ) : (
+        <div>
+          <img
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://via.placeholder.com/200x300?text=No+Poster"
+            }
+            style={{ width: "360px", height: "240px" }}
+            alt={movie.Title}
+          />
+          <h1>{"Title: " + movie.Title}</h1>
+          <p>{"Year: " + movie.Year}</p>
+          <p>{"Genre: " + movie.Genre}</p>
+          <p>{"Director: " + movie.Director}</p>
+          <p>{"Plot: " + movie.Plot}</p>
+          <p>{"Rating: " + movie.imdbRating}</p>
+        </div>
+      )}
+    </div>
+  );
 }
